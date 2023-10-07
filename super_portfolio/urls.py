414f2fb9206b957d01da8f5d16762db0ff1b07bd
anadolacio <anadolacio@hotmail.com>
2023-10-07 10:47:33 -0300

    resolução requisito 1

--- super_portfolio/urls.py
+++ super_portfolio/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenObtainPairView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
